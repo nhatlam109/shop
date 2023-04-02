ce8ef209e1142ba66a90d7233fb3fa3b79b89253
lstung219 <lstung2004@gmail.com>
2023-04-02 11:54:40 +0700

    chet vi cringe

--- web.js
+++ web.js
@@ -1,31 +1,8 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const bar = document.getElementById('bar');
+const nav = document.getElementById('navbar');
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+if(bar){
+  bar.addEventListener('click', () => {
+    nav.classList.add('active');
+  })
 }
-
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  if (n > slides.length) {slideIndex = 1}    
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slides[slideIndex-1].style.display = "block";  
-}
-
-var modal = document.getElementById('id01');
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
-
